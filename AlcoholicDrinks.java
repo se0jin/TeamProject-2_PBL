90b8561ae25343060213dd803d43d35500c78b8f
seoyun211 <seoyun_j211@naver.com>
2026-06-06 19:09:20 +0900

    기존 코드 삭제 및 교수님 디스크립션으로 해당 내용 전체 수정

--- AlcoholicDrinks.java
+++ AlcoholicDrinks.java
@@ -5,26 +5,13 @@
  * @author (2팀)
  * @version (2026.06.02)
  */
-public class AlcoholicDrinks extends Products implements TAX 
-{
-    private static final double TAX_RATE = 0.10;
- 
-    /**
-     * AlcoholicDrinks 생성자
-     *
-     * @param name  주류명
-     * @param price 주류 가격
-     */
-    public AlcoholicDrinks(String name, int price) {
-        super(name, price);
+public class AlcoholicDrinks extends Products implements TAX {
+    private static final double TAX_RATE = 0.1;   // 세율 10%
+
+    public AlcoholicDrinks(int productID, String name, int price) {
+        super(productID, name, price);
     }
- 
-    /**
-     * 세금 계산 메소드 오버라이딩
-     *
-     * @return 세금액 = price × 10%
-     */
-    @Override
+
     public double CalculateTax() {
         return price * TAX_RATE;
     }
